30986efecac3acca521c3abcb7896779021971c8
rTexty <kamil.axmadiev88@gmail.com>
2023-09-10 01:03:41 +0500

    new function

--- firstproject/views.py
+++ firstproject/views.py
@@ -4,7 +4,14 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+
+
+
 def reversed(request):
+    # num = request.GET['num_of_text']
     db = request.GET['text']
+    num = db.split()
     reversed_text = db[::-1]
-    return render(request, 'reversed.html', {'text':db, 'reversedtext': reversed_text})
+    return render(request, 'reversed.html', {'text':db, 'reversedtext': reversed_text, 'num': len(num),})
+
+
